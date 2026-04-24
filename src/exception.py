09a914d46478ec765abe9abc055eea4e4ee90e55
anujkc001 <chauhananujkumar278@gmail.com>
2026-04-24 10:18:04 +0530

    Data transformaition done

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,10 @@ from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}] ".format()
+    line_number = exc_tb.tb_lineno
+    # error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}] ".format()
+    # To this (ensure you pass the variables):
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}] ".format(file_name, line_number, str(error))
     file_name,exc_tb.tb_lineno,str(error)
 
     return error_message
